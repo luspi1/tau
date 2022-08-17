8e1ce99cfe40428ca95cc2fc88f6f77487ba88be
Владелец <igbabin566@gmail.com>
2022-08-17 17:37:19 +0300

    v1.3

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -2,5 +2,6 @@ const body = document.querySelector('body')
 const modalOverlay = document.querySelector('.modal-overlay')
 const modalSocials = document.querySelector('.modal-socials')
 const modalFullname = document.querySelector('.modal-fullname')
+const modalPassport = document.querySelector('.passport-modal')
 
-export { body, modalOverlay, modalSocials, modalFullname }
+export { body, modalOverlay, modalSocials, modalFullname, modalPassport }
--- src/js/components/passportForm.js
+++ src/js/components/passportForm.js
@@ -1,4 +1,67 @@
+import { body, modalOverlay, modalPassport } from "../_vars";
+
 const passportForm = document.querySelector('.passport-modal__form')
+
+
+const passportData = {
+  number: '',
+  dateIssue: '',
+  issuer: '',
+  constantCity: '',
+  constantIndex: '',
+  constantAddress: '',
+  temporaryCity: '',
+  temporaryIndex: '',
+  temporaryAddress: '',
+  actualCity: '',
+  actualIndex: '',
+  actualAddress: '',
+  typeRegistration: ''
+}
+
+// Обработка события отправки
+
+if (passportForm) {
+  passportForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const {
+      passportNumber,
+      dateIssue,
+      issuer,
+      constantCity,
+      constantIndex,
+      constantAddress,
+      temporaryCity,
+      temporaryIndex,
+      temporaryAddress,
+      typeReg,
+      actualCity,
+      actualIndex,
+      actualAddress
+    } = e.target.elements;
+
+    passportData.number = passportNumber.value
+    passportData.dateIssue = dateIssue.value
+    passportData.issuer = issuer.value
+    passportData.constantCity = constantCity.value
+    passportData.constantIndex = constantIndex.value
+    passportData.constantAddress = constantAddress.value
+    passportData.temporaryCity = temporaryCity.value
+    passportData.temporaryIndex = temporaryIndex.value
+    passportData.temporaryAddress = temporaryAddress.value
+    passportData.actualCity = actualCity.value
+    passportData.actualIndex = actualIndex.value
+    passportData.actualAddress = actualAddress.value
+    passportData.typeRegistration = typeReg.value
+
+
+    modalPassport.classList.remove('_active')
+    modalOverlay.classList.remove('modal-overlay_active')
+    body.classList.remove('_lock')
+
+  })
+}
+
 
 // Валидация
 
