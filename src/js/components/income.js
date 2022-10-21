e17cc4a6ab79f0b7459bba57b6a05bb4caca21ed
Владелец <igbabin566@gmail.com>
2022-10-21 17:14:43 +0300

    v4.8.4 доделана логика переключения между модалками + адаптив модалок

--- src/js/components/income.js
+++ src/js/components/income.js
@@ -1,3 +1,5 @@
+const incomePageMain = document.querySelector('.income-page')
+
 //аккордеоны
 
 const accordions = document.querySelectorAll('.months__accordion');
@@ -18,10 +20,24 @@ if (accordions) {
 
 // модалка прихода
 const incomingFileBtn = document.querySelector('.modal-incoming__file-wrapper input')
+const incomingManualFill = document.querySelector('.modal-incoming__manual-fill')
+const incomingFileFill = document.querySelector('.modal-incoming__file-fill')
+const incomingReturnBtn = document.querySelector('.modal-incoming__import-return-btn')
 
 if (incomingFileBtn) {
   incomingFileBtn.addEventListener('change', (e) => {
     e.target.value = ''
-    console.log('hello')
+    incomingManualFill.classList.remove('modal-incoming__manual-fill_active')
+    incomingFileFill.classList.add('modal-incoming__file-fill_active')
+  })
+}
+
+if (incomingReturnBtn) {
+  incomingReturnBtn.addEventListener('click', (e) => {
+    incomingManualFill.classList.add('modal-incoming__manual-fill_active')
+    incomingFileFill.classList.remove('modal-incoming__file-fill_active')
+
+    const incomingModal = document.querySelector('#incoming-modal')
+    incomePageMain.style.minHeight = `${incomingModal.clientHeight}px`
   })
 }
