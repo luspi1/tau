15dc85dc8b716daac9a2149b80f9e1c06b6801fa
VensKramskoy <venskramskoy@gmail.com>
2022-10-28 14:21:28 +0300

    закончен адаптив, стилизация календаря  на странице /движение средств/ и  стилизация календаря  на странице /доходы организации/

--- src/js/components/income.js
+++ src/js/components/income.js
@@ -41,3 +41,43 @@ if (incomingReturnBtn) {
     incomePageMain.style.minHeight = `${incomingModal.clientHeight}px`
   })
 }
+
+
+// настройка стилей календаря + на странице /income-traffic/
+
+const filterInputCalendars = document.querySelectorAll('.filter__calendar-input');
+const filterCalendarWrappers = document.querySelectorAll('.filter__calendar-wrap');
+
+const trafficInputCalendars = document.querySelectorAll('.traffic__calendar-input');
+const trafficCalendarWrappers = document.querySelectorAll('.traffic__calendar-wrap');
+
+if (filterInputCalendars || trafficInputCalendars ) {
+function showCustomCalendar(calendarInput, calendarWrap) {
+  calendarInput.forEach((inputDate, index) => {
+    inputDate.addEventListener('focusout', () => {
+      if (inputDate.value != '') {
+        calendarWrap[index].classList.add('active')
+      }
+    })
+  });
+}
+
+showCustomCalendar(filterInputCalendars, filterCalendarWrappers);
+showCustomCalendar(trafficInputCalendars, trafficCalendarWrappers);
+
+function hideCustomCalendar(calendarWrap, calendarInput) {
+  calendarWrap.forEach((parent, index) => {
+    parent.addEventListener('click', (event) => {
+      const target = event.target;
+     if (target && target.classList.contains('calendar-cross-close') ) {
+      calendarWrap[index].classList.remove('active');
+      calendarInput[index].value = '';
+      }
+    });
+  });
+}
+
+hideCustomCalendar(filterCalendarWrappers, filterInputCalendars);
+hideCustomCalendar(trafficCalendarWrappers, trafficInputCalendars);
+};
+
