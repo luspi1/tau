35813fb2cd847ca260c9031aa5c6fb4b417d2949
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-03-01 11:41:24 +0300

    v.8.5.3 Модалка добавления платежа приведена к одному виду, изменен функционал работы этой модалки на deal-balance и income

--- src/js/components/dealBalanceModal.js
+++ src/js/components/dealBalanceModal.js
@@ -5,85 +5,70 @@ import { initPaymentModalSelects }                       from './customSelect'
 import { initDateInputMasks }                            from './inputMask'
 import { initCloseModals }                               from './managePopup'
 
-const dealsPageMain = document.querySelector('.deals-balance-page')
+const initPaymentModal = () => {
+  initPaymentModalSelects()
+  initAllDates()
+  initDateInputMasks()
+  initCloseModals()
+  const addPaymentFileBtn = document.querySelector('.add-payment__file-wrapper input')
+  const addPaymentManualFill = document.querySelector('.add-payment__manual-fill')
+  const addPaymentFileFill = document.querySelector('.add-payment__file-fill')
+  const addPaymentReturnBtn = document.querySelector('.add-payment__import-return-btn')
 
-if (dealsPageMain) {
-  const initPaymentModal = () => {
-
-    initPaymentModalSelects()
-    initAllDates()
-    initDateInputMasks()
-    initCloseModals()
-
-    const addPaymentFileBtn = document.querySelector('.add-payment__file-wrapper input')
-    const addPaymentManualFill = document.querySelector('.add-payment__manual-fill')
-    const addPaymentFileFill = document.querySelector('.add-payment__file-fill')
-    const addPaymentReturnBtn = document.querySelector('.add-payment__import-return-btn')
-
-    const requiredFields = document.querySelectorAll('.add-payment input[data-required]')
-    if (addPaymentFileBtn) {
-      addPaymentFileBtn.addEventListener('change', (e) => {
-        e.target.value = ''
-        addPaymentManualFill.classList.remove('add-payment__manual-fill_active')
-        addPaymentFileFill.classList.add('add-payment__file-fill_active')
-        toggleRequiredFields(requiredFields)
-      })
-    }
-    if (addPaymentReturnBtn) {
-      addPaymentReturnBtn.addEventListener('click', (e) => {
-        addPaymentManualFill.classList.add('add-payment__manual-fill_active')
-        addPaymentFileFill.classList.remove('add-payment__file-fill_active')
-        const addPaymentModal = document.querySelector('#add-payment-modal')
-        dealsPageMain.style.minHeight = `${addPaymentModal.clientHeight}px`
-        toggleRequiredFields(requiredFields)
-      })
-    }
+  const requiredFields = document.querySelectorAll('.add-payment input[data-required]')
+  if (addPaymentFileBtn) {
+    addPaymentFileBtn.addEventListener('change', (e) => {
+      e.target.value = ''
+      addPaymentManualFill.classList.remove('add-payment__manual-fill_active')
+      addPaymentFileFill.classList.add('add-payment__file-fill_active')
+      toggleRequiredFields(requiredFields)
+    })
   }
-
-  const addPaymentBtn = document.querySelector('.deals-balance__add-btn')
-  const paymentId = addPaymentBtn.dataset.paymentId
-  const paymentScript = addPaymentBtn.dataset.paymentScript
-  const paymentModal = dealsPageMain.querySelector('#add-payment-modal')
-
-  addPaymentBtn.addEventListener('click', async () => {
-    const paymentData = {id_payment: paymentId}
-    const jsonPaymentData = JSON.stringify(paymentData)
-    const response = await sendData(jsonPaymentData, paymentScript)
-    const finishedResponse = await response.json()
-
-    const {status, errortext, html} = finishedResponse
-
-    if (status === 'ok') {
-      paymentModal.innerHTML = html
-      initPaymentModal()
-    } else {
-      showInfoModal(errortext)
-    }
-  })
+  if (addPaymentReturnBtn) {
+    addPaymentReturnBtn.addEventListener('click', (e) => {
+      addPaymentManualFill.classList.add('add-payment__manual-fill_active')
+      addPaymentFileFill.classList.remove('add-payment__file-fill_active')
+      toggleRequiredFields(requiredFields)
+    })
+  }
 }
 
 
-//Связка значения инпут с контентом
-//
-//   const dealName = document.querySelector('.deals-balance__list-content.deal-name')?.textContent
-//   const dealContrAgent = document.querySelector('.deals-balance__deal-contragent')?.textContent
-//   const dealMyCompany = document.querySelector(
-//     '.deals-balance__list-content.deal-company')?.textContent
-//
-//   const inputMyCompany = document.querySelector('.add-payment__input[name="add-payment_myCompany"]')
-//   const inputContrAgent = document.querySelector('.add-payment__input[name="add-payment_agent"]')
-//   const inputDealName = document.querySelector('.add-payment__input[name="add-payment_deal-title"]')
-//
-//
-//   if (dealsAddBtn) {
-//     dealsAddBtn.addEventListener('click', () => {
-//       inputMyCompany.value = dealMyCompany
-//       inputContrAgent.value = dealContrAgent
-//       inputDealName.value = dealName
-//     })
-//   }
+// Работа модалки на странице deal-balance.html
 
+const dealsPageMain = document.querySelector('.deals-balance-page')
 
+if (dealsPageMain) {
+  initPaymentModal()
+}
 
+// Работа модалки на странице income.html
 
+const incomePageMain = document.querySelector('.income-page')
+
+if (incomePageMain) {
+
+  const admissionModal = incomePageMain.querySelector('#add-payment-modal')
+  const admissionButtons = document.querySelectorAll('.months__button')
+
+  admissionButtons.forEach(btn => {
+    const admissionBtnId = btn.dataset.paymentId
+    const admissionBtnScript = btn.dataset.paymentScript
+
+    btn.addEventListener('click', async () => {
+      const paymentData = {id_payment: admissionBtnId}
+      const jsonPaymentData = JSON.stringify(paymentData)
+      const response = await sendData(jsonPaymentData, admissionBtnScript)
+      const finishedResponse = await response.json()
+      const {status, errortext, html} = finishedResponse
+
+      if (status === 'ok') {
+        admissionModal.innerHTML = html
+        initPaymentModal()
+      } else {
+        showInfoModal(errortext)
+      }
+    })
+  })
+}
 
--- src/js/components/income.js
+++ src/js/components/income.js
@@ -4,7 +4,6 @@ import { toggleRequiredFields } from '../_functions'
 const incomePageMain = document.querySelector('.income-page')
 
 if (incomePageMain) {
-
 //аккордеоны
 
   const accordions = document.querySelectorAll('.months__accordion')
@@ -19,36 +18,6 @@ if (incomePageMain) {
           monthsItems[index].classList.toggle('active')
         }
       })
-    })
-  }
-
-
-// модалка прихода
-  const incomingFileBtn = document.querySelector('.modal-incoming__file-wrapper input')
-  const incomingManualFill = document.querySelector('.modal-incoming__manual-fill')
-  const incomingFileFill = document.querySelector('.modal-incoming__file-fill')
-  const incomingReturnBtn = document.querySelector('.modal-incoming__import-return-btn')
-
-  const requiredFields = document.querySelectorAll('.modal-incoming input[data-required]')
-
-
-  if (incomingFileBtn) {
-    incomingFileBtn.addEventListener('change', (e) => {
-      e.target.value = ''
-      incomingManualFill.classList.remove('modal-incoming__manual-fill_active')
-      incomingFileFill.classList.add('modal-incoming__file-fill_active')
-      toggleRequiredFields(requiredFields)
-
-    })
-  }
-
-  if (incomingReturnBtn) {
-    incomingReturnBtn.addEventListener('click', (e) => {
-      incomingManualFill.classList.add('modal-incoming__manual-fill_active')
-      incomingFileFill.classList.remove('modal-incoming__file-fill_active')
-      const incomingModal = document.querySelector('#incoming-modal')
-      incomePageMain.style.minHeight = `${incomingModal.clientHeight}px`
-      toggleRequiredFields(requiredFields)
     })
   }
 
@@ -91,53 +60,6 @@ if (incomePageMain) {
     hideCustomCalendar(trafficCalendarWrappers, trafficInputCalendars)
   }
 
-//Связка значения инпут с контентом
-
-  const monthsButtons = document.querySelectorAll('.months__button')
-
-  const incomeSelect = incomePageMain?.querySelector('.modal-incoming__select-wrapper-payment select')
-
-
-  const incomeChoices = new Choices(incomeSelect, {
-    itemSelectText: '',
-    searchEnabled: false,
-    shouldSort: false,
-    allowHTML: true
-  })
-
-
-  if (monthsButtons) {
-    monthsButtons.forEach(btn => {
-      btn.addEventListener('click', () => {
-        const incomeMonthCompany = document.querySelector(
-          '.title__select-wrap .choices__item.choices__item--selectable').textContent
-        const incomeMonthRow = btn.closest('.months__row')
-        const incomeMonthDeal = incomeMonthRow.querySelector('.months__deal').textContent
-        const incomeMonthAgent = incomeMonthRow.querySelector('.months__organization').textContent
-        const incomePayment = incomeMonthRow.querySelector('.months__payment').textContent
-
-        const inputCompany = document.querySelector(
-          '.modal-incoming__input[name="incoming_company-title"]')
-        const inputAgent = document.querySelector('.modal-incoming__input[name="incoming_agent"]')
-        const inputDealTitle = document.querySelector(
-          '.modal-incoming__input[name="incoming_deal-title"]')
-
-        const defaultValuesModal = (inputName, textContent) => {
-          inputName.value = textContent.replace(/ +/g, ' ').trim()
-          inputName.disabled = true
-          inputName.closest('.modal-incoming__input-wrapper').classList.add('input_disabled')
-        }
-
-        defaultValuesModal(inputCompany, incomeMonthCompany)
-        defaultValuesModal(inputAgent, incomeMonthAgent)
-        defaultValuesModal(inputDealTitle, incomeMonthDeal)
-
-        incomeChoices.destroy()
-        incomeChoices.init()
-        incomeChoices.setValue([{value: incomePayment, label: incomePayment}])
-      })
-    })
-  }
 }
 
 
