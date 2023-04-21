a95703fa5d1906b869555810c6ef726040c8c0c7
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-04-21 16:08:58 +0300

    v.9.5.6 правки

--- src/js/components/income-traffic.js
+++ src/js/components/income-traffic.js
@@ -1,12 +1,18 @@
 // фильтр
 
-const filterButtons = document.querySelectorAll('.traffic__btn[data-traffic]')
-const hiddenInput = document.querySelector('#traffic-hidden-input')
-filterButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    filterButtons.forEach(btn => btn.classList.remove('active'))
-    button.classList.add('active')
-    hiddenInput.value = button.dataset.traffic
+const incomeTrafficPage = document.querySelector('.income-traffic-page')
+
+if (incomeTrafficPage) {
+  const filterButtons = document.querySelectorAll('.traffic__btn[data-traffic]')
+  const hiddenInput = document.querySelector('#traffic-hidden-input')
+  filterButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      filterButtons.forEach(btn => btn.classList.remove('active'))
+      button.classList.add('active')
+      hiddenInput.value = button.dataset.traffic
+    })
   })
-})
+}
+
+
 
