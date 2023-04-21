478a0caefe7fd363ae9c05fd93da006bb0707e01
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-04-21 11:23:43 +0300

    v.9.5.5 правки

--- src/js/components/income-traffic.js
+++ src/js/components/income-traffic.js
@@ -2,17 +2,11 @@
 
 const filterButtons = document.querySelectorAll('.traffic__btn[data-traffic]')
 const hiddenInput = document.querySelector('#traffic-hidden-input')
-
-let dataButtonFilter = ''
-
 filterButtons.forEach(button => {
   button.addEventListener('click', (e) => {
-    filterButtons.forEach(btn => {
-      btn.classList.remove('active')
-    })
+    filterButtons.forEach(btn => btn.classList.remove('active'))
     button.classList.add('active')
-    dataButtonFilter = button.dataset.traffic
-    hiddenInput.value = dataButtonFilter
+    hiddenInput.value = button.dataset.traffic
   })
 })
 
