c05e69927eab0a2af76053e29b703b54cdf789c0
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-05-16 12:54:34 +0300

    v.8.03 добавлен скрипт удаления в correspondence-item.html

--- src/js/components/correspondenceItem.js
+++ src/js/components/correspondenceItem.js
@@ -1,11 +1,44 @@
-import { checkConfirm } from '../_functions'
+import { checkConfirm, sendData, showInfoModal, } from '../_functions'
+
 
 const corrItemPage = document.querySelector('.correspondence-item')
 
 if (corrItemPage) {
   const delBtn = corrItemPage.querySelector('.newmail-one__delete-btn')
+
+  const confirmDelBtn = corrItemPage.querySelector('.check-confirm-modal .btn._confirm')
+  const handleDeleteSubmit = async () => {
+    const data = {
+      id: delId
+    }
+    const jsonData = JSON.stringify(data)
+
+    try {
+      const response = await sendData(jsonData, delScript)
+      const finishedResponse = await response.json()
+
+      const {status, errortext} = finishedResponse
+      if (status === 'ok') {
+        window.location.href = delUrl
+      } else {
+        showInfoModal(errortext)
+      }
+    } catch (err) {
+      showInfoModal("Во время выполнения запроса произошла ошибка")
+      console.error(err)
+    }
+  }
+
+  const delScript = confirmDelBtn.dataset.script
+  const delId = confirmDelBtn.dataset.id
+  const delUrl = confirmDelBtn.dataset.url
+
   delBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    checkConfirm(() => console.log('письмо удалено'))
+    checkConfirm(() => null)
   })
+
+  confirmDelBtn.addEventListener('click', handleDeleteSubmit)
+
+
 }
