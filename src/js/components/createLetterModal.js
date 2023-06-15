61be1b68e8470589ab928697e9b3d566db1022d4
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-06-15 17:26:04 +0300

    показ/скрытие номера трека при типе "Бумажное" в модалке создания письма

--- src/js/components/createLetterModal.js
+++ src/js/components/createLetterModal.js
@@ -1,6 +1,9 @@
 const createLetterModal = document.querySelector('#create-letter-modal')
 
 if (createLetterModal) {
+
+
+  // показ/скрытие прикрепления квитанции при методе "Курьер"
   const shippingMethodSelect = createLetterModal.querySelector('.modal-create-letter__shipping-select')
   const receiptFileUpload = createLetterModal.querySelector('.modal-create-letter__receipt-file')
   shippingMethodSelect.addEventListener('change', (e) => {
@@ -8,6 +11,17 @@ if (createLetterModal) {
       receiptFileUpload.classList.remove('hidden')
     } else {
       receiptFileUpload.classList.add('hidden')
+    }
+  })
+
+  // показ/скрытие номера трека при типе "Бумажное"
+  const shippingTypeSelect = createLetterModal.querySelector('.modal-create-letter__type-select')
+  const trackInput = createLetterModal.querySelector('.modal-create-letter__track-input')
+  shippingTypeSelect.addEventListener('change', (e) => {
+    if (e.detail.value === '2') {
+      trackInput.classList.remove('hidden')
+    } else {
+      trackInput.classList.add('hidden')
     }
   })
 }
