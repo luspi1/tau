e195ad36d99581084b7d38e7fcd02c6b403abe0d
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-06-27 10:12:39 +0300

    модалка "операции с шаблоном" настроена передача данных после выбора шаблона

--- src/js/components/docTemplateModal.js
+++ src/js/components/docTemplateModal.js
@@ -1,3 +1,5 @@
+import { formToObj, serializeForm } from '../_functions'
+
 const docTemplateModal = document.querySelector('#doc-template-modal')
 
 if (docTemplateModal) {
@@ -11,6 +13,26 @@ if (docTemplateModal) {
       e.preventDefault()
       stateModal.dataset.state = e.currentTarget.dataset.stateBtn
     })
+  })
+
+  // обработка выбора шаблона (Использовать выбранный шаблон)
+
+  const choiceTemplateForm = docTemplateModal.querySelector('.modal-doc-template__form')
+
+  const templateDataInput = document.querySelector('.create-doc-page .create-doc-page__tmpl-input-data')
+  const templateInput = document.querySelector('.create-doc-page .create-doc-page__tmpl-input')
+  const tmplBasisLink = docTemplateModal.querySelector('.modal-doc-template__template-basis-link')
+
+  choiceTemplateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const templateValue = formToObj(serializeForm(e.currentTarget))
+    const templateRadio = choiceTemplateForm.querySelector(`input[value="${templateValue?.templateId}"]`)
+    const templateText = templateRadio?.closest('.radio-list__item')?.querySelector('label').textContent.trim()
+
+    templateDataInput.value = templateValue.templateId
+    templateInput.value = templateText
+    tmplBasisLink.textContent = templateText
+    stateModal.dataset.state = 'connected'
   })
 
 }
