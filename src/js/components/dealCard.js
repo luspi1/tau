897204eca5363600fff0d13c51305361f1e82d18
Kramskoy-v <kramskoy.va@gmail.com>
2023-07-17 16:38:51 +0300

    Появление модалки при клике на "создать договор" при отсутствии шаблона на стр deal-card.html

--- src/js/components/dealCard.js
+++ src/js/components/dealCard.js
@@ -1,18 +1,36 @@
-import { checkConfirm } from '../_functions'
+import {checkConfirm} from '../_functions'
 
 const dealCardPage = document.querySelector('.deal-card-page')
 
 if (dealCardPage) {
-  const createContractBtn = dealCardPage.querySelector('.one-deal__link-create')
+    const createContractBtn = dealCardPage.querySelector('.one-deal__link-create')
 
-  createContractBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const contractLink = e.target.href
-    const navigateByLink = () => {
-      window.location.href = contractLink
-    }
-    checkConfirm(navigateByLink, 'Вы действительно хотите создать договор?')
-  })
+    createContractBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        const caseTitle = document.querySelector('[data-caseTitle]')
+
+//проверка наличия шаблона сделки
+        if (caseTitle?.textContent.trim().length) {
+            const contractLink = e.target.href
+            const navigateByLink = () => {
+                window.location.href = contractLink
+            }
+            checkConfirm(navigateByLink, 'Вы действительно хотите создать договор?')
+        } else {
+            const infoModal = document.querySelector('.info-modal.modal')
+            infoModal.classList.remove('hidden')
+            infoModal.querySelector(
+                '.info-modal__content-text').innerHTML = 'Для продолжения работы загрузите <a href="#">шаблон договора</a>'
+            document.body.style.overflow = 'hidden'
+            infoModal.addEventListener('click', ({target})=>{
+                if (target && target === infoModal ){
+                    document.body.style.overflow = '';
+                    infoModal.classList.add('hidden')
+                }
+
+            })
+        }
+    })
 
 
 }
